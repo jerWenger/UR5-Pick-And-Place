2c0bd4c1e652392e59dbc1bc8348c66382da958b
Jeremy Wenger <jwenger@draper.com>
2025-04-07 12:00:22 -0400

    update test code to cycle through logical states to test actuator system

--- jw_code/test_esp.py
+++ jw_code/test_esp.py
@@ -1,8 +1,10 @@
 # test_esp.py
 
+import time
 from esp_interface.esp_interface import ESPInterface
 
 def test_esp():
+    counter = 0
     
     #Test connect
     try:
@@ -10,12 +12,24 @@ def test_esp():
         data = []
     except Exception as e:
         print("Cannot connect to ESP32:" + e)
-        exit
+        exit()
 
     while (True):
         got_data, data = esp.read_serial()
         print(got_data)
         print(data)
+
+        if (counter % 3):
+            esp.write_serial(1,0)
+        elif (counter % 2):
+            esp.write_serial(0,1)
+        else:
+            esp.write_serial(0,0)
+            
+        time.sleep(1)
+
+        counter += 1
+        
     
 
 if __name__ == "__main__":
