502b6e682519a3af8d417732292b641b17feab19
corrinab <corrinab@mit.edu>
2025-04-28 16:23:35 -0400

    integrated depth and color sensing

---
+++ jw_code/cv_interface/depth_integrated.py
@@ -0,0 +1,195 @@
+import cv2
+import pyrealsense2 as rs
+import numpy as np
+import bottle
+
+class CVInterface:
+    def __init__(self):
+        """
+        Initialize computer vision interface.
+        """
+        #self.cap = cv2.VideoCapture(0) # 0 for webcam, 1 for external camera
+
+        # Initialize pipeline and config
+        self.pipeline = rs.pipeline()
+        config = rs.config()
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+        # Start streaming
+        self.profile = self.pipeline.start(config)
+        depth_sensor = self.profile.get_device().first_depth_sensor()
+        self.depth_scale = depth_sensor.get_depth_scale()
+
+        # Align depth to color
+        align_to = rs.stream.color
+        self.align = rs.align(align_to)
+
+        # Set your area of interest in meters
+        self.depth_min = 0.8  # 80 cm
+        self.depth_max = 1.0  # 100 cm
+
+        # Define crop boundaries (top, bottom, left, right)
+        #y1, y2 = 90, 390    # vertical range
+        #x1, x2 = 170, 470   # horizontal range
+        
+        blue_lower_bound = (80,50,120)
+        blue_upper_bound = (110,255,255)
+        yellow_lower_bound = (26,20,120)
+        yellow_upper_bound = (40,255,255)
+        orange_lower_bound = (0,100,120) 
+        orange_upper_bound = (20,255,255)
+
+        self.colors = ["blue", "yellow", "orange"] #excludes clear
+        self.display_colors = {"blue": (0, 255, 0), "yellow": (0, 250, 250), "orange": (25, 25, 255), "clear": (130, 0, 75)}
+        self.color_ranges = {"blue": (blue_lower_bound, blue_upper_bound), 
+                             "yellow": (yellow_lower_bound, yellow_upper_bound),
+                             "orange": (orange_lower_bound, orange_upper_bound)}
+        self.kernel = np.ones((5,5),np.uint8)
+        #self.pixels_per_inch = 17
+        self.ppm = 671.9
+        self.belt_space = (140, 0, 530, 430) #X1, Y1, X2, Y2
+
+    def get_camera_fps(self):
+        return self.cap.get(cv2.CAP_PROP_FPS)
+    
+    def get_camera_resolution(self):
+        """
+        Get width and height of camera stream
+        """
+        width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+        return width, height
+
+    def set_camera_fps(self, fps):
+        self.cap.set(cv2.CAP_PROP_FPS, fps)
+
+    def set_camera_resolution(self, width, height):
+        """
+        Set width and height of camera stream
+        """
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+    def map_cropped_to_original(self, pixelx, pixely):
+        left, top, right, bottom = self.belt_space
+    
+    def pixels_to_coordinates(self, pixelx, pixely):
+        """takes the x and y pixel indices from camera and returns
+        the coordinates in ur5 frame of reference"""
+        robotx = (pixely-23.5)/self.ppm
+        roboty = (pixelx+self.belt_space[0]-335)/self.ppm
+        return robotx, roboty
+
+    def find_centroid(self, c, display, color):
+            # compute the center of the contour
+            M = cv2.moments(c)
+            if M["m10"] != 0 and M["m00"] != 0 and M["m01"] != 0 and M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+
+                # draw the contour and center of the shape on the image
+                cv2.drawContours(display, [c], -1, self.display_colors[color], 2)
+                cv2.circle(display, (cX, cY), 7, self.display_colors[color], -1)
+                cv2.putText(display, color, (cX - 20, cY - 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                theta = 0.5*np.arctan2(2*M["mu11"],M["mu20"]-M["mu02"])
+                startX = int(cX - 200 * np.cos(theta)) 
+                startY = int(cY - 200 * np.sin(theta))
+                endX = int(200 * np.cos(theta) + cX) 
+                endY = int(200 * np.sin(theta) + cY)
+                cv2.line(display, (startX, startY), (endX,endY), self.display_colors[color], 5)
+
+            return display, cX, cY, theta
+    
+    def bottle_identification(self):
+        """
+        use camera to identify bottle location and output a display of where the bottle is
+        """
+        results = [] #list of identified bottles - (color, centerX, centerY, theta)
+
+        # Wait for a new frame
+        frames = self.pipeline.wait_for_frames()
+        aligned_frames = self.align.process(frames)
+
+        depth_frame = aligned_frames.get_depth_frame()
+        color_frame = aligned_frames.get_color_frame()
+    
+        # Convert frames to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        left, top, right, bottom = self.belt_space
+        depth_image = depth_image[top:bottom, left:right] #crop image
+        color_image = color_image[top:bottom, left:right]
+        display = color_image.copy()
+
+        hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
+
+        for color in self.colors:
+            bounds = self.color_ranges[color]
+            mask = cv2.inRange(hsv, bounds[0], bounds[1])
+        
+            #opening?
+            closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, self.kernel, iterations = 1)
+            cnts, _ = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            if len(cnts) > 0:
+                c = max(cnts, key = cv2.contourArea)
+                if cv2.contourArea(c) >= 225: #experiment with minimum area
+                    display, X, Y, theta = self.find_centroid(c, display, color)
+                    realX, realY = self.pixels_to_coordinates(X, Y)
+                    results.append((color, realX, realY, theta))
+
+        if not results:
+            # Convert depth to meters
+            depth_meters = depth_image * self.depth_scale
+            # Clip to range of interest (0.8m to 1.0m)
+            depth_clipped = np.clip(depth_meters, self.depth_min, self.depth_max)
+            # Normalize clipped depth to 0–255
+            depth_normalized = cv2.normalize(depth_clipped, None, 0, 255, cv2.NORM_MINMAX)
+            #depth_colormap = cv2.applyColorMap(depth_normalized.astype(np.uint8), cv2.COLORMAP_JET) #for display
+            depth_blurred = cv2.GaussianBlur(depth_normalized, (5, 5), 0)
+            mask = cv2.inRange(depth_blurred, 0, 124)
+            
+            #add opening and closing
+            contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            if len(contours) > 0:
+                c = max(contours, key = cv2.contourArea)
+                if cv2.contourArea(c) >= 225: #experiment with minimum area
+                    display, X, Y, theta = self.find_centroid(c, display, "clear")
+                    realX, realY = self.pixels_to_coordinates(X, Y)
+                    results.append(("clear", realX, realY, theta))
+            
+
+        first = self.first_bottle(results)
+        if first:
+            return bottle.Bottle(*first), display
+        else:
+            return None, display
+    
+    def first_bottle(self, results):
+        if not results:
+            return ()
+        else:
+            return min(results, key=lambda x: x[2])
+        
+if __name__=="__main__":
+    CV = CVInterface()
+    #CV.set_camera_resolution(640, 480)
+    #CV.set_camera_fps(10)
+    while True:
+        results, display = CV.bottle_identification()
+        #print(results)
+        
+        # show the image
+        cv2.imshow("Output", display)
+        #cv2.waitKey(3)
+
+        #press q to quit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    CV.pipeline.stop()
+    cv2.destroyAllWindows()
